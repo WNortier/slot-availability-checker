a30ead9456ebc0ef0685acf22a433870dea5ccdf
Warwick Nortier <warwick.nortier@gmail.com>
2020-10-14 01:43:18 +0200

    edited langing page

--- slot-checker/slot-checker-dom.js
+++ slot-checker/slot-checker-dom.js
@@ -1,60 +1,62 @@
-const calcTimesBtn = document.querySelector("#calc-times");
-const calcDateBuffer = document.querySelector("#calc-date-buffer");
-const calcIsDateListedBtn = document.querySelector("#calc-is-date-listed");
-const calctimeslotBtn = document.querySelector("#calc-is-slot-listed");
-const calcIsJobLengthGreaterThanTimeslotBtn = document.querySelector(
-  "#calc-is-job-length"
-);
-const timeslot = document.querySelector("#timeslot");
-const jobLength = document.querySelector("#jobLength");
-const date = document.querySelector(".date");
-const availability = document.querySelector("#availability");
-const datesBufferResult = document.querySelector("#dates-buffer-result");
-const timeResult = document.querySelector("#time-result");
-const isDateListedResult = document.querySelector("#is-date-listed-result");
-const timeslotResult = document.querySelector("#is-slot-listed-result");
-const isJoblengthGreaterResult = document.querySelector(
-  "#is-jobLength-greater-result"
-);
-const slotChecker = SlotChecker();
+document.addEventListener("DOMContentLoaded", () => {
+  const calcTimesBtn = document.querySelector("#calc-times");
+  const calcDateBuffer = document.querySelector("#calc-date-buffer");
+  const calcIsDateListedBtn = document.querySelector("#calc-is-date-listed");
+  const calctimeslotBtn = document.querySelector("#calc-is-slot-listed");
+  const calcIsJobLengthGreaterThanTimeslotBtn = document.querySelector(
+    "#calc-is-job-length"
+  );
+  const timeslot = document.querySelector("#timeslot");
+  const jobLength = document.querySelector("#jobLength");
+  const date = document.querySelector(".date");
+  const availability = document.querySelector("#availability");
+  const datesBufferResult = document.querySelector("#dates-buffer-result");
+  const timeResult = document.querySelector("#time-result");
+  const isDateListedResult = document.querySelector("#is-date-listed-result");
+  const timeslotResult = document.querySelector("#is-slot-listed-result");
+  const isJoblengthGreaterResult = document.querySelector(
+    "#is-jobLength-greater-result"
+  );
+  const slotChecker = SlotChecker();
 
-calcDateBuffer.addEventListener("click", () => {
-  datesBufferResult.textContent = slotChecker.handleSameDateBuffer(
-    timeslot.value,
-    jobLength.value,
-    date.value,
-    [9, 10, 14, 15, 16, 17]
-  );
-});
-calcTimesBtn.addEventListener("click", () => {
-  timeResult.textContent = slotChecker.handleTravelBuffer(
-    timeslot.value,
-    jobLength.value,
-    date.value,
-    [9, 10, 14, 15, 16, 17]
-  );
-});
-calcIsDateListedBtn.addEventListener("click", () => {
-  isDateListedResult.textContent = slotChecker.isDateListed(
-    timeslot.value,
-    jobLength.value,
-    date.value,
-    [9, 10, 14, 15, 16, 17]
-  );
-});
-calctimeslotBtn.addEventListener("click", () => {
-  timeslotResult.textContent = slotChecker.isSlotListed(
-    timeslot.value,
-    jobLength.value,
-    date.value,
-    [9, 10, 14, 15, 16, 17]
-  );
-});
-calcIsJobLengthGreaterThanTimeslotBtn.addEventListener("click", () => {
-  isJoblengthGreaterResult.textContent = slotChecker.isTimeslotLengthGreatherThanJobLength(
-    timeslot.value,
-    jobLength.value,
-    date.value,
-    [9, 10, 14, 15, 16, 17]
-  );
+  calcDateBuffer.addEventListener("click", () => {
+    datesBufferResult.textContent = slotChecker.handleSameDateBuffer(
+      timeslot.value,
+      jobLength.value,
+      date.value,
+      [9, 10, 14, 15, 16, 17]
+    );
+  });
+  calcTimesBtn.addEventListener("click", () => {
+    timeResult.textContent = slotChecker.handleTravelBuffer(
+      timeslot.value,
+      jobLength.value,
+      date.value,
+      [9, 10, 14, 15, 16, 17]
+    );
+  });
+  calcIsDateListedBtn.addEventListener("click", () => {
+    isDateListedResult.textContent = slotChecker.isDateListed(
+      timeslot.value,
+      jobLength.value,
+      date.value,
+      [9, 10, 14, 15, 16, 17]
+    );
+  });
+  calctimeslotBtn.addEventListener("click", () => {
+    timeslotResult.textContent = slotChecker.isSlotListed(
+      timeslot.value,
+      jobLength.value,
+      date.value,
+      [9, 10, 14, 15, 16, 17]
+    );
+  });
+  calcIsJobLengthGreaterThanTimeslotBtn.addEventListener("click", () => {
+    isJoblengthGreaterResult.textContent = slotChecker.isTimeslotLengthGreatherThanJobLength(
+      timeslot.value,
+      jobLength.value,
+      date.value,
+      [9, 10, 14, 15, 16, 17]
+    );
+  });
 });
